7fc1342ec3c93c9f74d19927f66d7cce84a7dbbd
john <ydbook@ymail.com>
2020-07-01 17:25:00 +0800

    one more thing_new3

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "lizy_aws_allow_ssh_web" {
 resource "aws_instance" "amazon_linux_2" {
   ami = "ami-0a1c2ec61571737db"
   instance_type = "t2.micro"
-  key_name = "lizy_aws"
+  key_name = "lizy_tokyo"
   subnet_id = aws_subnet.lizy_outside_subnet.id
   iam_instance_profile = "WebService"
   security_groups = [aws_security_group.lizy_aws_allow_ssh_web.id]
